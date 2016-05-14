613dae8112fe468cfdcb1e44157bfbf0fd01b577
Andrea Leopardi <an.leopardi@gmail.com>
2016-05-14 19:19:46 +0200

    Add code and tests for the :make compiler

---
+++ test/fixtures/my_app/mix.exs
@@ -0,0 +1,8 @@
+defmodule MyApp.Mixfile do
+  use Mix.Project
+
+  def project do
+    [app: :my_app,
+     version: "1.0.0"]
+  end
+end
---
+++ test/mix/tasks/compile.make_test.exs
@@ -0,0 +1,118 @@
+defmodule Mix.Tasks.Compile.MakeTest do
+  use ExUnit.Case
+
+  import Mix.Tasks.Compile.Make, only: [run: 1]
+  import ExUnit.CaptureIO
+
+  @fixture_project Path.expand("../../fixtures/my_app", __DIR__)
+
+  defmodule Sample do
+    def project do
+      [app: :sample,
+       version: "0.1.0"]
+    end
+  end
+
+  setup do
+    in_fixture(fn -> File.rm_rf!("Makefile") end)
+    :ok
+  end
+
+  test "running with a specific executable" do
+    in_fixture fn ->
+      with_project_config [make_executable: "nonexistentmake"], fn ->
+        assert_raise Mix.Error, "`nonexistentmake` not found in the current path", fn ->
+          run([])
+        end
+      end
+    end
+  end
+
+  test "running without a makefile" do
+    msg = ~r/\ACould not compile with/
+
+    in_fixture fn ->
+      File.rm_rf!("Makefile")
+
+      capture_io fn ->
+        assert_raise Mix.Error, msg, fn -> run([]) end
+      end
+    end
+  end
+
+  test "running with a makefile" do
+    in_fixture fn ->
+      File.write! "Makefile", """
+      target:
+      \t@echo "hello"
+      """
+
+      assert capture_io(fn -> run([]) end) =~ "hello\n"
+    end
+  end
+
+  test "specifying targets" do
+    in_fixture fn ->
+      File.write! "Makefile", """
+      useless_target:
+      \t@echo "nope"
+      target:
+      \t@echo "target"
+      other_target:
+      \t@echo "other target"
+      """
+
+      with_project_config [make_targets: ~w(target other_target)], fn ->
+        output = capture_io(fn -> run([]) end)
+        assert output =~ "target\n"
+        assert output =~ "other target\n"
+        refute output =~ "nope"
+      end
+    end
+  end
+
+  test "specifying a cwd" do
+    in_fixture fn ->
+      File.mkdir_p!("subdir")
+      File.write! "subdir/Makefile", """
+      all:
+      \t@echo "subdir"
+      """
+
+      with_project_config [make_cwd: "subdir"], fn ->
+        assert capture_io(fn -> run([]) end) == "subdir\n"
+      end
+    end
+  end
+
+  test "specifying a makefile" do
+    in_fixture fn ->
+      File.write "MyMakefile", """
+      all:
+      \t@echo "my makefile"
+      """
+
+      with_project_config [make_makefile: "MyMakefile"], fn ->
+        assert capture_io(fn -> run([]) end) == "my makefile\n"
+      end
+    end
+  end
+
+  test "specifying a custom error message" do
+    in_fixture fn ->
+      with_project_config [make_error_message: "try harder"], fn ->
+        capture_io fn ->
+          assert_raise Mix.Error, ~r/try harder/, fn -> run([]) end
+        end
+      end
+    end
+  end
+
+  defp in_fixture(fun) do
+    File.cd!(@fixture_project, fun)
+  end
+
+  defp with_project_config(config, fun) do
+    Mix.Project.in_project(:my_app, @fixture_project, config, fn(_) -> fun.() end)
+  end
+end
