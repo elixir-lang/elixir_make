64f6c8187036ab10d3658145b4227b6bde2a12e9
José Valim <jose.valim@plataformatec.com.br>
2016-10-27 17:04:39 +0200

    Release v0.4.0

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule ElixirMake.Mixfile do
 
   def project do
     [app: :elixir_make,
-     version: "0.3.0",
+     version: "0.4.0",
      elixir: "~> 1.1",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
