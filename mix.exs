c8285de328033e3619b7fdb6be37163678bc9757
José Valim <jose.valim@plataformatec.com.br>
2019-06-19 20:28:41 +0200

    Add docs generator

--- mix.exs
+++ mix.exs
@@ -1,14 +1,17 @@
 defmodule ElixirMake.Mixfile do
   use Mix.Project
 
+  @version "0.6.0"
+
   def project do
     [
       app: :elixir_make,
-      version: "0.6.0",
+      version: @version,
       elixir: "~> 1.3",
       description: "A Make compiler for Mix",
       package: package(),
-      deps: []
+      docs: docs(),
+      deps: [{:ex_doc, "~> 0.20", only: :docs}]
     ]
   end
 
@@ -25,5 +28,13 @@ defmodule ElixirMake.Mixfile do
       links: %{"GitHub" => "https://github.com/elixir-lang/elixir_make"},
       maintainers: ["Andrea Leopardi", "José Valim"]
     }
+  end
+
+  defp docs do
+    [
+      main: "Mix.Tasks.Compile.ElixirMake",
+      source_ref: "v#{@version}",
+      source_url: "https://github.com/elixir-lang/elixir_make"
+    ]
   end
 end
