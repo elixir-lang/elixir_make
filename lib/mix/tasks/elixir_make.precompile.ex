8857680033a3dfece8b84bcb559592199572222d
Cocoa <r.xu.1@research.gla.ac.uk>
2022-07-29 21:35:41 +0100

    updated inline docs

--- lib/mix/tasks/elixir_make.precompile.ex
+++ lib/mix/tasks/elixir_make.precompile.ex
@@ -2,41 +2,68 @@ defmodule Mix.Tasks.ElixirMake.Precompile do
   use Mix.Task
 
   @doc """
-  The precompiler should compile for the host natively
+  This callback will be invoked when the user executes the `mix compile`
+  (or `mix compile.elixir_make`) command.
+
+  The precompiler should then compile the NIF library "natively". Note that
+  it is possible for the precompiler module to pick up other environment variables
+  like `TARGET_ARCH=aarch64` and adjust compile arguments correspondingly.
   """
   @callback build_native(OptionParser.argv()) :: :ok | {:ok, []} | no_return
 
   @doc """
+  This callback will be invoked when the NIF library is trying to load functions
+  from its shared library.
+
   The precompiler should download or reuse nif file for current target.
 
   ## Paramters
 
     - `context`: Precompiler context returned by the `precompiler_context` callback.
+
   """
-  @callback download_or_reuse_nif_file(context :: term()) :: :ok | {:error, String.t()}
+  @callback download_or_reuse_nif_file(context :: term()) :: :ok | {:error, String.t()} | no_return
 
   @doc """
-  Precompiler context
-  """
-  @callback precompiler_context(OptionParser.argv()) :: term()
+  This callback will be invoked when the user executes the following commands:
 
-  @doc """
-  Post actions to run after all precompiling tasks
-  """
-  @callback post_precompile(context :: term()) :: :ok
+  - `mix elixir_make.fetch --all`
+  - `mix elixir_make.fetch --all --print`
 
-  @doc """
-  Returns URLs for NIFs based on its module name.
-  The module name is the one that defined the NIF and this information
-  is stored in a metadata file.
+  The precompiler module should return all available URLs to precompiled artefacts
+  of the NIF library.
   """
   @callback available_nif_urls() :: [String.t()]
 
   @doc """
-  Returns the file URL to be downloaded for current target.
-  It receives the NIF module.
+  This callback will be invoked when the user executes the following commands:
+
+  - `mix elixir_make.fetch --only-local`
+  - `mix elixir_make.fetch --only-local --print`
+
+  The precompiler module should return the URL to a precompiled artefact of
+  the NIF library for current target (the "native" host).
   """
   @callback current_target_nif_url() :: String.t()
+
+  @doc """
+  This optional callback is designed to store the precompiler's state or context.
+
+  The returned value will be used in the `download_or_reuse_nif_file/1` and
+  `post_precompile/1` callback.
+  """
+  @callback precompiler_context(OptionParser.argv()) :: term()
+
+  @doc """
+  This optional callback will be invoked when all precompilation tasks are done,
+  i.e., it will only be called at the end of the `mix elixir_make.precompile`
+  command.
+
+  Post actions to run after all precompilation tasks are done. For example,
+  actions can be archiving all precompiled artefacts and uploading the archive
+  file to an object storage server.
+  """
+  @callback post_precompile(context :: term()) :: :ok
 
   @impl true
   def run(args) do
@@ -44,6 +71,7 @@ defmodule Mix.Tasks.ElixirMake.Precompile do
   end
 
   defp ensure_precompiler_module!(module) do
+    module = Module.concat([Mix.Tasks.ElixirMake, module])
     if Code.ensure_loaded?(module) do
       module
     else
@@ -52,10 +80,21 @@ defmodule Mix.Tasks.ElixirMake.Precompile do
   end
 
   defp precompile(args, module) when module != nil do
-    module = ensure_precompiler_module!(Module.concat([Mix.Tasks.ElixirMake, module]))
-    Kernel.apply(module, :run, [args])
-    context = Kernel.apply(module, :precompiler_context, [args])
-    Kernel.apply(module, :post_precompile, [context])
+    module = ensure_precompiler_module!(module)
+    ret = Kernel.apply(module, :run, [args])
+
+    context =
+      if Kernel.function_exported?(module, :precompiler_context, 1) do
+        Kernel.apply(module, :precompiler_context, [args])
+      else
+        nil
+      end
+
+    if Kernel.function_exported?(module, :post_precompile, 1) do
+      Kernel.apply(module, :post_precompile, [context])
+    else
+      ret
+    end
   end
 
   def build_native(args) do
@@ -67,7 +106,7 @@ defmodule Mix.Tasks.ElixirMake.Precompile do
   end
 
   defp precompile_build_native(args, module) do
-    module = ensure_precompiler_module!(Module.concat([Mix.Tasks.ElixirMake, module]))
+    module = ensure_precompiler_module!(module)
     Kernel.apply(module, :build_native, [args])
   end
 
@@ -80,7 +119,7 @@ defmodule Mix.Tasks.ElixirMake.Precompile do
   end
 
   defp available_nif_urls(module) do
-    module = ensure_precompiler_module!(Module.concat([Mix.Tasks.ElixirMake, module]))
+    module = ensure_precompiler_module!(module)
     Kernel.apply(module, :available_nif_urls, [])
   end
 
@@ -93,7 +132,7 @@ defmodule Mix.Tasks.ElixirMake.Precompile do
   end
 
   defp download_or_reuse_nif_file(context, module) do
-    module = ensure_precompiler_module!(Module.concat([Mix.Tasks.ElixirMake, module]))
+    module = ensure_precompiler_module!(module)
     Kernel.apply(module, :download_or_reuse_nif_file, [context])
   end
 
@@ -106,7 +145,7 @@ defmodule Mix.Tasks.ElixirMake.Precompile do
   end
 
   def current_target_nif_url(module) do
-    module = ensure_precompiler_module!(Module.concat([Mix.Tasks.ElixirMake, module]))
+    module = ensure_precompiler_module!(module)
     Kernel.apply(module, :current_target_nif_url, [])
   end
 end
